c8d0966bb0bb3a71a52d6a50390cd251bb320242
Evgenii Astapov <astae556@newschool.edu>
2026-03-31 15:03:52 -0400

    7th

--- popup.js
+++ popup.js
@@ -1,1 +1,26 @@
 console.log("This is a popup!")
+
+// Removed content_scripts from original demo manifest.json since I will use executeScript
+// Why executeScript? My popup.js will control the outcome
+// The extension will not work automatically after the page loads
+// It will work after User opened my popu.js by clicking on the extension icon
+// Also executeScript can return data to my popup.js. On contrary content_scripts requires sendMessage to do so
+
+async function getTabId() {
+    // async means 'There are something you need to wait for'
+    // await means 'Wait for me'
+    const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+    return tab.id;
+}
+
+async function scanTab() {
+    // Wait until line 9_getTabId() is finished and return it as a variable here
+    const tabId = await getTabId();
+    const result = await chrome.scripting.executeScript({
+        target: { tabId: tabId },
+        func: () => {
+            return document.title; // Your script content here
+        }
+    });
+    console.log(result.result); // Check if it works
+}
