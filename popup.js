696b5a6cee55e8fa3b53c858e322237c421db0be
Evgenii Astapov <astae556@newschool.edu>
2026-03-31 17:34:32 -0400

    element to el
    should have kept it short at first place as I will combine it in Set, etc

--- popup.js
+++ popup.js
@@ -22,17 +22,17 @@ async function scanTab() {
             const allElements = document.querySelectorAll("*"); // Select all the elements in the tab. Spirce: https://developer.mozilla.org/en-US/docs/Web/API/Document/all
             const data = []; // Now I need an empty array to store upcoming data. Source: https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Array
 
-            allElements.forEach((element) => { // I need to run through all the element in the tab. Source: https://developer.mozilla.org/en-US/docs/Web/API/Window/getComputedStyle
-                const elementStyle = getComputedStyle(element); // This picks the element and returns its properties data applied in CSS. Source: https://developer.mozilla.org/en-US/docs/Web/API/Window/getComputedStyle. ChatGPT link where I accidentally found this function: https://chatgpt.com/share/69cc26c7-e5ac-8325-80d5-40bb5e8833c0
+            allElements.forEach((el) => { // I need to run through all the element in the tab. Source: https://developer.mozilla.org/en-US/docs/Web/API/Window/getComputedStyle
+                const elStyle = getComputedStyle(el); // This picks the element and returns its properties data applied in CSS. Source: https://developer.mozilla.org/en-US/docs/Web/API/Window/getComputedStyle. ChatGPT link where I accidentally found this function: https://chatgpt.com/share/69cc26c7-e5ac-8325-80d5-40bb5e8833c0
                 data.push({ // While im running throug the elements in line 25, this picks data array and add elements to the end of data array until it runs out of the elements described in lines 28-34. Source: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/push
                     // ChatGPT link about CSS data i can potentially get: https://chatgpt.com/share/69cc26c7-e5ac-8325-80d5-40bb5e8833c0
-                    color: elementStyle.color,
-                    backgroundColor: elementStyle.backgroundColor,
-                    borderColor: elementStyle.borderColor,
-                    fontFamily: elementStyle.fontFamily,
-                    fontSize: elementStyle.fontSize,
-                    fontWeight: elementStyle.fontWeight,
-                    lineHeight: elementStyle.lineHeight
+                    color: elStyle.color,
+                    backgroundColor: elStyle.backgroundColor,
+                    borderColor: elStyle.borderColor,
+                    fontFamily: elStyle.fontFamily,
+                    fontSize: elStyle.fontSize,
+                    fontWeight: elStyle.fontWeight,
+                    lineHeight: elStyle.lineHeight
                 });
             });
 
