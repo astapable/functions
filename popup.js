3202faa04ba3acf1d40c30ee5d0b271abec21cf8
Evgenii Astapov <astae556@newschool.edu>
2026-04-14 23:49:08 -0400

    Update popup.js

--- popup.js
+++ popup.js
@@ -178,7 +178,7 @@ async function scanTab() {
         `
         <li>
             <div class="color-box" style="--bg-color:${rgbStringToHex(el)};">
-                <h3 style="color:${getContrastColor(rgbStringToHex(el))}">${rgbStringToHex(el)}</h3>
+                <h4 style="color:${getContrastColor(rgbStringToHex(el))}">${rgbStringToHex(el)}</h4>
                 <p class="footnote" style="color:${getContrastColor(rgbStringToHex(el))}">${colorInstance[el] || 0} instances</p>
             </div>  
         </li>
